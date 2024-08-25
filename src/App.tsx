2ec6fca9250df2a037a19dab8e564769469f326c
nicolas772 <nicolas.arayau@sansano.usm.cl>
2024-08-24 21:56:45 -0400

    feat: Añadir forma de borrar todos los TODOs completados

--- src/App.tsx
+++ src/App.tsx
@@ -48,6 +48,11 @@ const App = (): JSX.Element => {
     setFilterSelected(filter)
   }
 
+  const handleRemoveAllCompleted = () : void => {
+    const newTodos = todos.filter(todo => !todo.completed)
+    setTodos(newTodos)
+  }
+
   const filteredTodos = todos.filter(todo => {
     if (filterSelected === TODO_FILTERS.ACTIVE) return !todo.completed
     if (filterSelected === TODO_FILTERS.COMPLETED) return todo.completed
@@ -68,7 +73,7 @@ const App = (): JSX.Element => {
         completedCount={completedCount}
         filterSelected={filterSelected}
         handleFilterChange={handleFilterChange}
-        onClearCompleted={() => {}}
+        onClearCompleted={handleRemoveAllCompleted}
       ></Footer>
     </div>
   )
